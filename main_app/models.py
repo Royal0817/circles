6bbf1b6c65c54febf78dce11984061079199727c
royal0817 <martinezl27737@gmail.com>
2022-06-06 12:21:32 -0400

    static login page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class UserR(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+    
